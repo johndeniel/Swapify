edd54c887db6f5750907a7bbaf75723d402eb609
johndeniel <johndeniel.d37@gmail.com>
2026-09-13 12:43:21 +0800

    feat: add 12 new platforms and sort all alphabetically

--- app/src/main/java/com/akin/wallet/fragment/SocialLoginsFragment.java
+++ app/src/main/java/com/akin/wallet/fragment/SocialLoginsFragment.java
@@ -319,23 +319,33 @@ public class SocialLoginsFragment extends Fragment {
 
     private List<PlatformOption> getPlatforms() {
         List<PlatformOption> platforms = new ArrayList<>();
+        platforms.add(new PlatformOption(R.drawable.binance, "Binance", "binance.com"));
+        platforms.add(new PlatformOption(R.drawable.bdo, "BDO", "bdo.com.ph"));
+        platforms.add(new PlatformOption(R.drawable.bpi, "BPI", "bpi.com.ph"));
         platforms.add(new PlatformOption(R.drawable.discord, "Discord", "discord.com"));
         platforms.add(new PlatformOption(R.drawable.dropbox, "Dropbox", "dropbox.com"));
         platforms.add(new PlatformOption(R.drawable.facebook, "Facebook", "facebook.com"));
+        platforms.add(new PlatformOption(R.drawable.gcash, "GCash", "gcash.com"));
         platforms.add(new PlatformOption(R.drawable.gitlab, "GitLab", "gitlab.com"));
         platforms.add(new PlatformOption(R.drawable.github, "GitHub", "github.com"));
         platforms.add(new PlatformOption(R.drawable.gmail, "Gmail", "gmail.com"));
+        platforms.add(new PlatformOption(R.drawable.gotyme, "GoTyme", "gotyme.com"));
         platforms.add(new PlatformOption(R.drawable.google, "Google", "google.com"));
         platforms.add(new PlatformOption(R.drawable.instagram, "Instagram", "instagram.com"));
         platforms.add(new PlatformOption(R.drawable.itunes, "iTunes", "itunes.com"));
+        platforms.add(new PlatformOption(R.drawable.lazada, "Lazada", "lazada.com"));
         platforms.add(new PlatformOption(R.drawable.line, "LINE", "line.me"));
         platforms.add(new PlatformOption(R.drawable.linkedin, "LinkedIn", "linkedin.com"));
+        platforms.add(new PlatformOption(R.drawable.maribank, "MariBank", "maribank.com.ph"));
+        platforms.add(new PlatformOption(R.drawable.maya, "Maya", "maya.ph"));
         platforms.add(new PlatformOption(R.drawable.messenger, "Messenger", "messenger.com"));
         platforms.add(new PlatformOption(R.drawable.microsoft, "Microsoft", "microsoft.com"));
         platforms.add(new PlatformOption(R.drawable.netflix, "Netflix", "netflix.com"));
         platforms.add(new PlatformOption(R.drawable.paypal, "PayPal", "paypal.com"));
         platforms.add(new PlatformOption(R.drawable.pinterest, "Pinterest", "pinterest.com"));
+        platforms.add(new PlatformOption(R.drawable.rcbc, "RCBC", "rcbc.com.ph"));
         platforms.add(new PlatformOption(R.drawable.reddit, "Reddit", "reddit.com"));
+        platforms.add(new PlatformOption(R.drawable.shoopee, "Shopee", "shopee.com"));
         platforms.add(new PlatformOption(R.drawable.slack, "Slack", "slack.com"));
         platforms.add(new PlatformOption(R.drawable.snapchat, "Snapchat", "snapchat.com"));
         platforms.add(new PlatformOption(R.drawable.soundcloud, "SoundCloud", "soundcloud.com"));
@@ -344,9 +354,11 @@ public class SocialLoginsFragment extends Fragment {
         platforms.add(new PlatformOption(R.drawable.telegram, "Telegram", "telegram.org"));
         platforms.add(new PlatformOption(R.drawable.tiktok, "TikTok", "tiktok.com"));
         platforms.add(new PlatformOption(R.drawable.tinder, "Tinder", "tinder.com"));
+        platforms.add(new PlatformOption(R.drawable.unionbank, "UnionBank", "unionbank.com.ph"));
         platforms.add(new PlatformOption(R.drawable.viber, "Viber", "viber.com"));
         platforms.add(new PlatformOption(R.drawable.wattpad, "Wattpad", "wattpad.com"));
         platforms.add(new PlatformOption(R.drawable.whatsapp, "WhatsApp", "whatsapp.com"));
+        platforms.add(new PlatformOption(R.drawable.wise, "Wise", "wise.com"));
         platforms.add(new PlatformOption(R.drawable.x, "X", "x.com"));
         platforms.add(new PlatformOption(R.drawable.youtube, "YouTube", "youtube.com"));
         platforms.add(new PlatformOption(R.drawable.zoom, "Zoom", "zoom.us"));
