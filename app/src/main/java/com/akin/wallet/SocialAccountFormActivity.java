0fc52ffd2d075fb04593aec2bef48a7500d4cc63
johndeniel <johndeniel.d37@gmail.com>
2026-09-18 19:20:05 +0800

    chore: remove unused imports and dead bottom-nav style

--- app/src/main/java/com/akin/wallet/SocialAccountFormActivity.java
+++ app/src/main/java/com/akin/wallet/SocialAccountFormActivity.java
@@ -3,8 +3,6 @@ package com.akin.wallet;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
